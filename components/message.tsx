f45b002706f9d3cf94348d6135983fab08a95cad
AyakSalu <aewyheq@gmail.com>
2026-04-18 19:48:32 +0300

    remove bottom icon

--- components/message.tsx
+++ components/message.tsx
@@ -355,17 +355,6 @@ export const ThinkingMessage = () => {
       initial={{ opacity: 0 }}
       transition={{ duration: 0.2 }}
     >
-      <div className="flex items-start justify-start gap-3">
-        <div className="-mt-1 flex size-8 shrink-0 items-center justify-center rounded-full bg-background ring-1 ring-border">
-          <SparklesIcon size={14} />
-        </div>
-
-        <div className="flex w-full flex-col gap-2 md:gap-4">
-          <div className="p-0 text-muted-foreground text-sm">
-            Thinking...
-          </div>
-        </div>
-      </div>
     </motion.div>
   );
 };
